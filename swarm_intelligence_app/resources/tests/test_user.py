4d04ef7f2bb88bbbbf63646ec24f1610b27b78a1
moha187@hotmail.de <moha187@hotmail.de>
2016-11-18 15:46:46 +0100

    testuser

--- swarm_intelligence_app/resources/tests/test_user.py
+++ swarm_intelligence_app/resources/tests/test_user.py
@@ -1,18 +1,13 @@
 from unittest import TestCase
-from swarm_intelligence_app.resources.user import User
-from flask import Flask, json_available
-import httplib2
+from flask import Flask
 import urllib
-import json
 import requests
-import jsonschema
-from jsonschema import validate
 
 app = Flask(__name__)
 
-
 class TestUser(TestCase):
     token = "mock_user_001"
+    changeToken = "mock_user_002"
 
     def encode_param(self, firstname, lastname, email):
         params = urllib.parse.urlencode({'firstname': firstname,
@@ -21,117 +16,127 @@ class TestUser(TestCase):
 
         return params
 
-    # def test_delete_user(self):
-    #     conn = httplib2.Http(".cache")
-    #     (response, content) = conn.request("http://localhost:5000/drop", "GET")
-    #     self.assertEqual(response.status, 200, "drop database")
-    #     (response, content) = conn.request("http://localhost:5000/setup",
-    #                                        "GET")
-    #     self.assertEqual(response.status, 200, "setup database")
-    #     (response, content) = conn.request(
-    #         uri="http://localhost:5000/users",
-    #         method="POST",
-    #         headers={'Content-Type': 'application/x-www-form-urlencoded'},
-    #         body=TestUser.encode_param(
-    #             self, 'testName', 'testLastName', 'testEmail'))
-    #     self.assertEqual(response.status, 200)
-    #     (response, content) = conn.request("http://localhost:5000/users/1",
-    #                                        "DELETE")
-    #     self.assertEqual(response.status, 200)
-    #
-    #     (response, content) = conn.request("http://localhost:5000/users/1",
-    #                                        "DELETE")
-    #     self.assertEqual(response.status, 404)
-    #
-    # def test_get_user(self):
-    #     conn = httplib2.Http(".cache")
-    #     (response, content) = conn.request("http://localhost:5000/drop",
-    #                                        "GET")
-    #     self.assertEqual(response.status, 200, "drop database")
-    #     (response, content) = conn.request("http://localhost:5000/setup",
-    #                                        "GET")
-    #     self.assertEqual(response.status, 200, "setup database")
-    #     (response, content) = conn.request(
-    #          uri="http://localhost:5000/users",
-    #          method="POST",
-    #          headers={'Content-Type': 'application/x-www-form-urlencoded'},
-    #          body=TestUser.encode_param(self, 'testName', 'testLastName',
-    #                                     'testEmail'))
-    #     self.assertEqual(response.status, 200)
-    #     (response, content) = conn.request("http://localhost:5000/me",
-    #                                        "GET")
-    #     self.assertEqual(response.status, 200)
-    #
-    #     assert "1" in str(content)
-    #     assert "testName" in str(content)
-    #     assert "testLastName" in str(content)
-    #     assert "testEmail" in str(content)
-    #
-    #     (response, content) = conn.request("http://localhost:5000/drop",
-    #                                        "GET")
-    #     self.assertEqual(response.status, 200, "drop database")
-    #
+    def tearDown(self):
+        response = requests.get(url="http://localhost:5000/drop")
+        self.assertEqual(response.status_code, 200, "drop database")
+
+    def setUp(self):
+        response = requests.get(url="http://localhost:5000/drop")
+        self.assertEqual(response.status_code,200, "drop database")
+        response = requests.get(url="http://localhost:5000/setup")
+        self.assertEqual(response.status_code, 200, "setup database")
+
+
+    def test_delete_user(self):
+        # Post Request Execution
+        json_post_response = requests.request(
+            method='POST',
+            url="http://localhost:5000/me",
+            headers={'Authorization': 'Token ' + self.token}).json()['data']
+
+        first_name_post = json_post_response['firstname']
+        last_name_post = json_post_response['lastname']
+        email_post = json_post_response['email']
+        is_deleted = json_post_response['is_deleted']
+
+        self.assertFalse(is_deleted)
+        # Delete Request Execution
+        delete_response = requests.request(
+            method='DELETE',
+            url="http://localhost:5000/me",
+            headers={'Authorization': 'Token ' +  self.token}).json()['data']
+
+        json_get_response = requests.request(
+            method='GET',
+            url="http://localhost:5000/me",
+            headers={'Authorization': 'Token ' + self.token}).json()['data']
+
+        first_name_get= json_get_response['firstname']
+        last_name_get = json_get_response['lastname']
+        email_get = json_get_response['email']
+        is_deleted_get = json_get_response['is_deleted']
+
+        self.assertEqual(first_name_post,first_name_get)
+        self.assertEqual(last_name_post,last_name_get)
+        self.assertEqual(email_post,email_get)
+        self.assertTrue(is_deleted_get)
+
     def test_update_user(self):
-        conn = httplib2.Http(".cache")
-        (response, content) = conn.request("http://localhost:5000/drop",
-                                           "GET")
-        self.assertEqual(response.status, 200, "drop database")
-        (response, content) = conn.request("http://localhost:5000/setup",
-                                           "GET")
-        self.assertEqual(response.status, 200, "setup database")
+       # POST Request Execution
+        json_post_response = requests.request(
+            method='POST',
+            url="http://localhost:5000/me",
+            headers={'Authorization': 'Token ' + self.token}).json()['data']
 
-        (response, content) = conn.request(
-            uri="http://localhost:5000/me",
-            method="POST",
-            headers={'Content-Type': 'application/x-www-form-urlencoded',
-                     'Authorization': 'Token ' + self.token})
+        first_name_post = json_post_response['firstname']
+        last_name_post = json_post_response['lastname']
+        email_post = json_post_response['email']
+        id_post = json_post_response['google_id']
 
-        self.assertEqual(response.status, 200)
+        # PUT Request Execution
+        json_put_response = requests.request(
+            method='PUT',
+            url="http://localhost:5000/me",
+            headers={'Authorization': 'Token ' + self.token},
+            params={'firstname': 'Daisy',
+                    'lastname': 'Ducks',
+                    'email': 'daisy@tolli.com'}
+        ).json()['data']
 
-        # userName = content['firstname']
-        #    userLastName = content['lastname']
-        #    userEmail = content['email']
-        # print("userName= " + userName)
+        first_name_put = json_put_response['firstname']
+        last_name_put = json_put_response['lastname']
+        email_put = json_put_response['email']
+        id_put = json_put_response['google_id']
 
-        print("==========================================")
-        jsonResponse = requests.request(
+        # ASSERTION Block POST&PUT
+        self.assertNotEqual(first_name_post, first_name_put)
+        self.assertNotEqual(last_name_post, last_name_put)
+        self.assertNotEqual(email_post, email_put)
+
+        # GET Request Execution
+        json_response = requests.request(
             method='GET',
             url="http://localhost:5000/me",
             headers={'Authorization': 'Token ' + self.token}).json()[
             'data']
 
-        firstName = jsonResponse['firstname']
-        lastName = jsonResponse['lastname']
-        email = jsonResponse['email']
-        id = jsonResponse['google_id']
+        first_name = json_response['firstname']
+        last_name = json_response['lastname']
+        email = json_response['email']
+        id = json_response['google_id']
 
-
-        print(firstName + lastName + email + id)
-        print("==========================================")
-
-        # self.assertNotEquals(userName,content['firstname'])
-        # self.assertNotEquals(userLastName, content['lastname'])
-        # self.assertNotEquals(userEmail, content['email'])
-
-        (response, content) = conn.request("http://localhost:5000/drop", "GET")
-        self.assertEqual(response.status, 200, "drop database")
+        # ASSERTION Block PUT&GET
+        self.assertEqual(first_name_put, first_name)
+        self.assertEqual(last_name_put, last_name)
+        self.assertEqual(email_put, email)
+        self.assertEqual(id_put, id)
 
     def test_post_users(self):
-        conn = httplib2.Http(".cache")
-        (response, content) = conn.request("http://localhost:5000/drop", "GET")
-        self.assertEqual(response.status, 200, "drop database")
-        (response, content) = conn.request("http://localhost:5000/setup",
-                                           "GET")
-        self.assertEqual(response.status, 200, "setup database")
+        # POST Request Execution
+        json_post_response = requests.request(
+            method='POST',
+            url="http://localhost:5000/me",
+            headers={'Authorization': 'Token ' + self.token}).json()['data']
 
-        (response, content) = conn.request(
-            uri="http://localhost:5000/me",
-            method="POST",
-            headers={'Content-Type': 'application/x-www-form-urlencoded',
-                     'Authorization': 'Token ' + self.token})
-        self.assertEqual(response.status, 200)
+        first_name_post = json_post_response['firstname']
+        last_name_post = json_post_response['lastname']
+        email_post = json_post_response['email']
+        id_post = json_post_response['google_id']
 
-        print("content= " + str(content))
+        # GET Request Execution
+        json_response = requests.request(
+            method='GET',
+            url="http://localhost:5000/me",
+            headers={'Authorization': 'Token ' + self.token}).json()[
+            'data']
 
-        (response, content) = conn.request("http://localhost:5000/drop", "GET")
-        self.assertEqual(response.status, 200, "drop database")
+        first_name = json_response['firstname']
+        last_name = json_response['lastname']
+        email = json_response['email']
+        id = json_response['google_id']
+
+        # ASSERT Block
+        self.assertEqual(first_name_post, first_name)
+        self.assertEqual(last_name_post, last_name)
+        self.assertEqual(email_post, email)
+        self.assertEqual(id_post, id)
