2ce175b705c3b39e59efb51e5daf64309f213a17
Felix Borst <felixborst94@gmail.com>
2016-11-22 16:02:13 +0100

    initial setup

--- runtests.py
+++
@@ -1,1 +0,0 @@
-DATABASE_URL=postgres://flaskexample:flask@localhost:5432/flaskexample_test nosetests --nocapture
--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 DATABASE_URL=postgres://flaskexample:flask@localhost:5432/flaskexample_test nosetests --nocapture
