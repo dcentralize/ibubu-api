da0bbfbaa56e04330773f929f702a6e4ab7f06bd
twaehlen <tobias.waehlen@googlemail.com>
2016-11-22 11:08:22 +0100

    unique constraint removed from field name

--- swarm_intelligence_app/models/organization.py
+++ swarm_intelligence_app/models/organization.py
@@ -3,7 +3,7 @@ from swarm_intelligence_app.models import db
 
 class Organization(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     is_deleted = db.Column(db.Boolean(), nullable=False)
 
     partners = db.relationship('Partner', backref='organization')
