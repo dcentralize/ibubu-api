bfdc142d332d6aceb151415a8e67aae62afa1f74
twaehlen <tobias.waehlen@googlemail.com>
2016-11-22 12:03:05 +0100

    docstrings fixed.

--- swarm_intelligence_app/resources/checklist.py
+++ swarm_intelligence_app/resources/checklist.py
@@ -1,25 +1,36 @@
+"""
+Define the classes for the checklist API.
+
+"""
 from flask_restful import Resource
 from swarm_intelligence_app.common import errors
 
 
 class Checklist(Resource):
+    """
+    Define the endpoints for the checklist node.
+
+    """
     def get(self,
             checklist_id):
         """
-        Retrieve a checklist
+        Retrieve a checklist.
+
         """
         raise errors.MethodNotImplementedError()
 
     def put(self,
             checklist_id):
         """
-        Edit a checklist
+        Edit a checklist.
+
         """
         raise errors.MethodNotImplementedError()
 
     def delete(self,
                checklist_id):
         """
-        Delete a checklist
+        Delete a checklist.
+
         """
         raise errors.MethodNotImplementedError()
--- swarm_intelligence_app/resources/metric.py
+++ swarm_intelligence_app/resources/metric.py
@@ -1,25 +1,36 @@
+"""
+Define the classes for the metric API.
+
+"""
 from flask_restful import Resource
 from swarm_intelligence_app.common import errors
 
 
 class Metric(Resource):
+    """
+    Define the endpoints for the metric node.
+
+    """
     def get(self,
             metric_id):
         """
-        Retrieve a metric
+        Retrieve a metric.
+
         """
         raise errors.MethodNotImplementedError()
 
     def put(self,
             metric_id):
         """
-        Edit a metric
+        Edit a metric.
+
         """
         raise errors.MethodNotImplementedError()
 
     def delete(self,
                metric_id):
         """
-        Delete a metric
+        Delete a metric.
+
         """
         raise errors.MethodNotImplementedError()
