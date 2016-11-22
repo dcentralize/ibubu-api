22964734a608241778a72d4ddb384df7c954fc80
twaehlen <tobias.waehlen@googlemail.com>
2016-11-22 11:47:46 +0100

    docstrings fixed.

--- swarm_intelligence_app/resources/partner.py
+++ swarm_intelligence_app/resources/partner.py
@@ -1,16 +1,24 @@
-from flask_restful import Resource, reqparse
-from swarm_intelligence_app.models import db
-from swarm_intelligence_app.models.partner import Partner as PartnerModel
+"""
+Define the classes for the partner API.
+
+"""
+from flask_restful import reqparse, Resource
 from swarm_intelligence_app.common import errors
 from swarm_intelligence_app.common.authentication import auth
+from swarm_intelligence_app.models import db
+from swarm_intelligence_app.models.partner import Partner as PartnerModel
 
 
 class Partner(Resource):
+    """
+    Define the endpoints for the partner node.
+
+    """
     @auth.login_required
     def get(self,
             partner_id):
         """
-        Retrieve a partner
+        Retrieve a partner.
 
         In order to retrieve a partner, the authenticated user must be a
         member or an admin of the organization that the partner is
@@ -18,6 +26,7 @@ class Partner(Resource):
 
         Params:
             partner_id: The id of the partner to retrieve
+
         """
         partner = PartnerModel.query.get(partner_id)
 
@@ -33,13 +42,14 @@ class Partner(Resource):
     def put(self,
             partner_id):
         """
-        Edit a partner
+        Edit a partner.
 
         In order to edit a partner, the authenticated user must be an admin of
         the organization that the partner is associated with.
 
         Params:
             partner_id: The id of the partner to edit
+
         """
         partner = PartnerModel.query.get(partner_id)
 
@@ -66,13 +76,14 @@ class Partner(Resource):
     def delete(self,
                partner_id):
         """
-        Delete a partner
+        Delete a partner.
 
         In order to delete a partner, the authenticated user must be an admin
         of the organization that the partner is associated with.
 
         Params:
             partner_id: The id of the partner to delete
+
         """
         partner = PartnerModel.query.get(partner_id)
 
@@ -89,32 +100,44 @@ class Partner(Resource):
 
 
 class PartnerMetrics(Resource):
+    """
+    Define the endpoints for the metrics edge of the partner node.
+
+    """
     def post(self,
              partner_id):
         """
-        Add a metric to a partner
+        Add a metric to a partner.
+
         """
         raise errors.MethodNotImplementedError()
 
     def get(self,
             partner_id):
         """
-        List metrics of a partner
+        List metrics of a partner.
+
         """
         raise errors.MethodNotImplementedError()
 
 
 class PartnerChecklists(Resource):
+    """
+    Define the endpoints for the checklists edge of the partner node.
+
+    """
     def post(self,
              partner_id):
         """
-        Add a checklist to a partner
+        Add a checklist to a partner.
+
         """
         raise errors.MethodNotImplementedError()
 
     def get(self,
             partner_id):
         """
-        List checklists of a partner
+        List checklists of a partner.
+
         """
         raise errors.MethodNotImplementedError()
