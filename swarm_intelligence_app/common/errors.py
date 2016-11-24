2ecdd839e7c402d3c99ff9daebcab55a9b30ab2b
twaehlen <tobias.waehlen@googlemail.com>
2016-11-24 01:32:29 +0100

    docstrings fixed.

--- swarm_intelligence_app/common/errors.py
+++ swarm_intelligence_app/common/errors.py
@@ -5,11 +5,25 @@ Define any error classes for the application.
 
 
 class EntityNotFoundError(Exception):
-    def __init__(self, type, id):
+    """
+    Define an EntityNotFoundError.
+
+    """
+    def __init__(self,
+                 type,
+                 id):
+        """
+        Initialize an EntityNotFoundError.
+
+        """
         self.type = type
         self.id = id
 
     def __str__(self):
+        """
+        Return an EntityNotFoundError as a readable message.
+
+        """
         return 'The {type} with id {id} does not exist'.format(
             type=self.type,
             id=self.id
@@ -17,25 +31,64 @@ class EntityNotFoundError(Exception):
 
 
 class MethodNotImplementedError(Exception):
-    def __init__(self, message=None):
+    """
+    Define an MethodNotImplementedError.
+
+    """
+    def __init__(self,
+                 message=None):
+        """
+        Initialize an MethodNotImplementedError.
+
+        """
         self.message = message
 
     def __str__(self):
+        """
+        Return an MethodNotImplementedError as a readable message.
+
+        """
         return self.message or 'The method you requested is not implemented ' \
                                'right now.'
 
 
 class EntityAlreadyExistsError(Exception):
-    def __init__(self, message=None):
+    """
+    Define an EntityAlreadyExistsError.
+
+    """
+    def __init__(self,
+                 message=None):
+        """
+        Initialize an EntityAlreadyExistsError.
+
+        """
         self.message = message
 
     def __str__(self):
+        """
+        Return an EntityAlreadyExistsError as a readable message.
+
+        """
         return self.message or 'The specified entity already exists.'
 
 
 class EntityNotModifiedError(Exception):
-    def __init__(self, message=None):
+    """
+    Define an EntityNotModifiedError.
+
+    """
+    def __init__(self,
+                 message=None):
+        """
+        Initialize an EntityNotModifiedError.
+
+        """
         self.message = message
 
     def __str__(self):
+        """
+        Return an EntityNotModifiedError as a readable message.
+
+        """
         return self.message or 'The specified entity cannot be modified.'
