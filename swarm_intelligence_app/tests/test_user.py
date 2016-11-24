0010b6e9c6bb21df7d2f3d2d1236aebb3a81115f
Felix Borst <felixborst94@gmail.com>
2016-11-24 14:24:45 +0100

    worked on user tests

--- swarm_intelligence_app/tests/test_user.py
+++ swarm_intelligence_app/tests/test_user.py
@@ -11,21 +11,41 @@ class TestUser:
     """
     token = 'mock_user_001'
 
-    def test_client(self, client):
+    def test_signin(self, client):
         """
         Test if the signin-page returns a valid http status-code.
         """
         assert client.get(url_for('signin')).status == '200 OK'
 
-    def test_db(self, client):
+    def test_setup(self, client):
         """
         Test if the setup-page returns a valid http status-code.
         """
         assert client.get(url_for('setup')).status == '200 OK'
 
-    def test_post(self, client):
+    def test_populate(self, client):
+        """
+        Test if the populate-page returns a valid http status-code.
+        """
+        assert client.get(url_for('populate')).status == '200 OK'
+
+    def test_me_post(self, client):
         """
         Test if the me-page returns a valid http status-code.
         """
         assert client.post('/me', headers={
+            'Authorization': 'Token ' + self.token}).status == '200 OK'
+
+    def test_me_get(self, client):
+        """
+        Test if the me-page returns a valid http status-code.
+        """
+        assert client.get('/me', headers={
+            'Authorization': 'Token ' + self.token}).status == '200 OK'
+
+    def test_me_put(self, client):
+        """
+        Test if the me-page returns a valid http status-code.
+        """
+        assert client.put('/me', headers={
             'Authorization': 'Token ' + self.token}).status == '200 OK'
