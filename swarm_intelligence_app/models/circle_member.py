87ccafc0c305f216c5582011cc731bbf3177bcdf
twaehlen <tobias.waehlen@googlemail.com>
2016-11-25 02:08:23 +0100

    circle model and circle resource implemented.

--- swarm_intelligence_app/models/circle_member.py
+++ swarm_intelligence_app/models/circle_member.py
@@ -8,5 +8,6 @@ from swarm_intelligence_app.models import db
 circle_members = db.Table(
     'circle_members',
     db.Column('partner_id', db.Integer, db.ForeignKey('partner.id')),
-    db.Column('circle_id', db.Integer, db.ForeignKey('circle.id'))
+    db.Column('circle_id', db.Integer, db.ForeignKey(
+        'circle.id', onupdate='CASCADE', ondelete='CASCADE'))
 )
